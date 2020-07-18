049ab677926a8834f96c8358fdef20cb6bba26d8
psyched189 <55517665+psyched189@users.noreply.github.com>
2020-07-18 00:30:56 -0500

    test_file_1

---
+++ main_dir/test_file_1.py
@@ -0,0 +1,22 @@
+from test_package import Configuration, stack, linked_list
+
+ll = linked_list.LinkedList()
+st = stack.Stack()
+config = Configuration()
+
+for i in range(1,11):
+	ll.append(i)
+
+ll.head.print_nodes()
+
+for i in range(11,21):
+	st.push(i)
+
+st.print_stack()
+st.pop()
+st.print_stack()
+
+config.add(10,0)
+config.add(20,1)
+
+config.print_config()
