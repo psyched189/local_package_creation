bdec6970501183530ed016095b4873a4fd80288d
psyched189 <55517665+psyched189@users.noreply.github.com>
2020-07-18 00:23:16 -0500

    add config class to init

--- main_dir/test_package/__init__.py
+++ main_dir/test_package/__init__.py
@@ -1,1 +1,11 @@
+class Configuration:
+    def __init__(self):
+        self.map = {}
 
+    def add(self, key, val):
+        self.map[key] = val
+
+    def print_config(self):
+        for k, v in self.map.items():
+            print(f'{k}:{v}')
+            
