28d1aaa23ccf4b2dc3974e216de5b280b0e48e63
psyched189 <55517665+psyched189@users.noreply.github.com>
2020-07-18 00:05:23 -0500

    create setup.py

---
+++ main_dir/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='test_package_1',
+      version='0.0.1',
+      description='Test project for creating packages',
+      packages=['test_pkg_ys1']
+      )
